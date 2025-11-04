a1c0566121367847362c010c53e387b6536805c2
Linkthero <125348071+Linkthero@users.noreply.github.com>
2025-11-04 18:58:57 +0100

    se añaden assets, y movimiento del personaje

--- ProyectoUnity2D/Assets/Scripts/PlayerMove.cs
+++ ProyectoUnity2D/Assets/Scripts/PlayerMove.cs
@@ -1,16 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField] private float velocidad;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * velocidad;
+    }
+
+    public void OnMove(InputValue inputvalue) 
+    {
+        moveInput = inputvalue.Get<Vector2>();    
         
     }
 }
